1f3964b5ea11073b9ccd44cd28afe4bba4139948
zyy <l>
2020-03-05 16:12:42 +0800

    2号程序员的jackson修改操作

--- src/main/java/com/itszt/JacksonConfig.java
+++ src/main/java/com/itszt/JacksonConfig.java
@@ -30,4 +30,15 @@ public class JacksonConfig {
         });
         return objectMapper;
     }
+
+//    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+//        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+//        objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+//            @Override
+//            public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+//                jsonGenerator.writeString("");
+//            }
+//        });
+//        return objectMapper;
+//    }
 }
